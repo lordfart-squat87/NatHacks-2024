a9c8e768a0c98eeb766799ff27ed47b85f08491f
Andrei Fabon <andrei17ig@gmail.com>
2024-11-14 16:30:10 -0700

    Update scratch.py

--- scratch.py
+++ scratch.py
@@ -4,8 +4,10 @@ print('Learning how to push and commit')
 for x in range(4):
     print('test')
 
+"""
 user_input = input("Enter Name: ")
 print("You entered:", user_input)
+"""
 
 print('Yoink')
 print('This is inportant')
@@ -14,3 +16,12 @@ for idea in idea:
     print(idea)
 
 #Nick was here
+
+x = 10
+
+while x > 0:
+    print(x)
+
+    x = x - 1
+
+
