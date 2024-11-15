18bdf1fde9f323175ce3c28ff978d0fc41bb6bba
Andrei Fabon <andrei17ig@gmail.com>
2024-11-15 04:29:59 -0700

    Update main.py

--- main.py
+++ main.py
@@ -1,3 +1,28 @@
-#pip install numpy scipy matplotlib sklearn pyserial
+"""
+#downloaded anaconda as there are issues bulding my dependices on my local laptop
 
+# remember to download these you need to open an anaconda prompt to download these libraries 
+#conda install numpy scipy matplotlib scikit-learn pyserial
 #this was used to download the necessary libraries to be used in the algortim to sort and isolate the waves 
+
+#in the ancaonda prompt 
+#conda activate myenv
+
+#input this in after I do the other steps
+#python
+
+Python 3.x.x | Anaconda, Inc. | (default, ...)
+Type "help", "copyright", "credits" or "license" for more information. #will get this if done correctly
+
+#input all these
+import numpy
+import scipy
+import matplotlib
+import sklearn
+import serial
+print("All libraries are installed successfully!")
+and if there are no errors then all libraries were downloaded properly
+
+#input this at the end to exit anaconda Prompt
+exit()
+"""
